fd2e9fa24b5c744624319dea76c749b0f7538d2a
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-09-03 07:04:10 +0100

    Insert row into the Friends table

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -16,9 +16,9 @@ connection = pymysql.connect(host='localhost',
 try:
     #Run a query
     with connection.cursor() as cursor:
-      sql = "SELECT * FROM Genre;"
-      cursor.execute("""CREATE TABLE IF NOT EXISTS
-                    Friends(name char(20), age int, DOB datetime);""")
+      row = ("Bob", 21, "1990-02-06 23:04:56")
+      cursor.execute("INSERT INTO Friends VALUES(%s, %s, %s);", row)
+      connection.commit()
 
 finally:
     # Close the connection, regardless of whether the above was successful
